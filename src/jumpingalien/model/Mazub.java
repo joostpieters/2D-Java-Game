e4b2a8c63a9bc7d69aa65a5c3216ca59f68d16b7
Stijn Caerts <StijnCaerts@users.noreply.github.com>
2015-02-25 14:42:37 +0100

    Mazub.java

--- src/jumpingalien/model/Mazub.java
+++ src/jumpingalien/model/Mazub.java
@@ -1,5 +1,32 @@
 package jumpingalien.model;
 
+import jumpingalien.util.Sprite;
+
 public class Mazub {
 
+	public Mazub(int pixelLeftX, int pixelBottomY, Sprite[] sprites){
+		this.location[0] = pixelLeftX;
+		this.location[1] = pixelBottomY;
+		this.setSprites(sprites);	
+	}
+	
+	public Sprite[] getSprites() {
+		return sprites;
+	}
+	public void setSprites(Sprite[] sprites) {
+		this.sprites = sprites;
+	}
+	
+	public void setLocation(int pixelLeftX, int pixelBottomY) {
+		this.location[0] = pixelLeftX;
+		this.location[1] = pixelBottomY;
+	}
+	
+	public int[] getLocation() {
+		return this.location;
+	}
+
+	// location[0] is x location, location[1] is y location
+	private int[] location = new int[2];
+	private Sprite[] sprites;
 }
