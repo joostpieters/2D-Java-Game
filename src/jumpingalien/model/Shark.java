4fd94559bbdd2ededbc9e29dcfc2c32da4de1c06
Pieter-Jan Coenen <pieterjan.coenen@student.kuleuven.be>
2015-04-10 17:04:06 +0200

    Sharks zijn beter

--- src/jumpingalien/model/Shark.java
+++ src/jumpingalien/model/Shark.java
@@ -180,6 +180,164 @@ public class Shark {
 		this.hitpoints = points;
 	}
 	
+	/**
+	 * this variable contains the amount of hitpoints for this shark
+	 */
 	private int hitpoints;
+	
+	/**
+	 * @return 	...
+	 * 			|result == this.velocityX
+	 */
+	private double getVelocityX() {
+		return velocityX;
+	}
+
+	/**
+	 * 
+	 * @param 	velocityX
+	 * @post	...
+	 * 			|new.getVelocityX() == velocityX
+	 */
+	private void setVelocityX(double velocityX) {
+		this.velocityX = velocityX;
+	}
+	
+	/**
+	 * This variable contains the horizontal velocity of this shark
+	 */
+	private double velocityX;
+	
+	/**
+	 * 
+	 * @return	...
+	 * 			| result == this.velocityX
+	 */
+	private double getVelocityY() {
+		return velocityY;
+	}
+
+	/**
+	 * 
+	 * @param velocityY
+	 * @post 	...
+	 * 			| new.getVelocityY() == velocityY
+	 */
+	private void setVelocityY(double velocityY) {
+		this.velocityY = velocityY;
+	}
+	
+	/**
+	 * This variable contains the vertical velocity for this shark
+	 */
+	private double velocityY;
+	
+	
+	/**
+	 * 
+	 * @return	...
+	 * 			| result == this.acceleration
+	 */
+	private double getAccelerationX() {
+		return accelerationX;
+	}
+
+	/**
+	 * 
+	 * @param accelerationX
+	 * @post 	...
+	 * 			| this.getAccelerationX() == accelerationX
+	 */
+	private void setAccelerationX(double accelerationX) {
+		this.accelerationX = accelerationX;
+	}
+	
+	/**
+	 * This variable contains the current horizontal acceleration for this Mazub
+	 */
+	private double accelerationX;
+
+	/**
+	 * @return	...
+	 * 			| result == this.accelerationY;
+	 */
+	private double getAccelerationY() {
+		return accelerationY;
+	}
+
+	/**
+	 * 
+	 * @param accelerationY
+	 * 
+	 * @post	...
+	 * 			|new.getAccelerationY == accelerationY
+	 */
+	private void setAccelerationY(double accelerationY) {
+		this.accelerationY = accelerationY;
+	}
+	
+	/**
+	 * This variable contains the vertical acceleration of this shark
+	 */
+	private double accelerationY;
+	
+	void advanceTime(double seconds) throws IllegalArgumentException {
+		if (seconds < 0 || seconds >= 0.2) 
+			throw new IllegalArgumentException();
+		this.setTimer(this.getTimer() + seconds);
+		while(seconds > 0){
+			double dt1 = 0.2;
+			double dt2 = 0.2;
+			if((getVelocityX() != 0) || (getAccelerationX() != 0)){
+				dt1 = (0.01)/(Math.abs(getVelocityX())+Math.abs(getAccelerationX())*seconds);
+			}
+			if((getVelocityY() != 0) || (getAccelerationY() != 0)){
+				dt2 = (0.01)/(Math.abs(getVelocityY())+Math.abs(getAccelerationY())*seconds);
+			}
+			if((dt1 != 0.2) || (dt2 != 0.2)){
+				if(dt1 >= seconds && dt2 >= seconds){
+					advanceTimeCollisionDetect(seconds);
+					seconds = 0;
+				} else if(dt1 < dt2){
+					advanceTimeCollisionDetect(dt1);
+					seconds -= dt1;
+				} else if(dt2 <= dt1){
+					advanceTimeCollisionDetect(dt2);
+					seconds -= dt2;
+				}
+			} else {
+				seconds = 0;
+			}
+			
+		}
+	}
+	private void advanceTimeCollisionDetect(double dt){
+		// TODO 		
+	}
+	
+	/**
+	 * 
+	 * @return	...
+	 * 			| result == this.timer
+	 */
+	private double getTimer() {
+		return timer;
+	}
+
+	/**
+	 * 
+	 * @param time
+	 * @post	...
+	 * 			| new.getTimer() == time
+	 */
+	private void setTimer(double time) {
+		this.timer = time;
+	}
+	
+	/**
+	 * This variable contains the timer for the movements of this shark
+	 */
+	private double timer;
+		
 	
 }
