df71496c8900d983f7986cdbc548d5a1fe53a690
Stijn Caerts <StijnCaerts@users.noreply.github.com>
2015-05-07 10:34:22 +0200

    update object

--- src/jumpingalien/part3/programs/expressions/Object.java
+++ src/jumpingalien/part3/programs/expressions/Object.java
@@ -1,5 +1,9 @@
 package jumpingalien.part3.programs.expressions;
 
 public class Object extends Literal {
-
+	public Object(Object object) {
+		this.object = object;
+	}
+	
+	private Object object;
 }
