2227643f9ebd940cc1054f4b10d50e0ffaa6f409
Stijn Caerts <StijnCaerts@users.noreply.github.com>
2015-05-07 10:50:31 +0200

    direction fix

--- src/jumpingalien/part3/programs/expressions/Direction.java
+++ src/jumpingalien/part3/programs/expressions/Direction.java
@@ -3,10 +3,10 @@ package jumpingalien.part3.programs.expressions;
 import jumpingalien.part3.programs.SourceLocation;
 
 public class Direction extends Literal {
-	public Direction(Direction value, SourceLocation sourceLocation) {
+	public Direction(jumpingalien.part3.programs.IProgramFactory.Direction value, SourceLocation sourceLocation) {
 		super(sourceLocation);
 		this.direction = value;
 	}
 	
-	private Direction direction;
+	private jumpingalien.part3.programs.IProgramFactory.Direction direction;
 }
